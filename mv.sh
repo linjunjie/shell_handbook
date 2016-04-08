1adfa88c441b042d70eefdbb191c4c33e516dc0c
linjunjie <linjunjie0707@gmail.com>
2016-04-08 12:05:42 +0800

    规范格式;添加注释

--- mv.sh
+++ mv.sh
@@ -3,5 +3,5 @@
 #@keyword 	mv,shopt,extglob
 #@env 		linux,osx
 #@assume	如果有平行的目录a,b,c,d,e,f,g,如果想将除a和b之外的目录都移动到a
-shopt -s extglob
+shopt -s extglob	#这里是打开bash shell的extglob选项，以支持更多的模式匹配操作符
 mv !(a|b) ./a
